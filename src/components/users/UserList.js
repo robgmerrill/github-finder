f1b94a9aa4e2a0bf89807caf6ce9421af1307334
Rob Merrill <robgmerrill@gmail.com>
2023-03-06 15:19:03 -0800

    finsished component

---
+++ src/components/users/UserList.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+function UserList() {
+  const [users, setUsers ] = useState([])
+//   const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    fetchUsers();
+  }, []) 
+
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    }})
+    const data = await response.json();
+    setUsers(data)
+    // setIsLoading(false)
+  }
+
+  console.log(users)
+//   if (!isLoading) {
+      return (
+        <div className='grid sm:grid-cols-1 lg:grid-cols-3'>
+         {users && users.map((user) => (
+              <h3 key={user.login}>{user.login}</h3>
+            ))}
+        </div>
+      )
+//   } else {
+//     return <p>...loading</p>
+//   }
+}
+
+export default UserList
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserList from '../components/users/UserList'
 
 function Home() {
   return (
     <div>
       <h1 className='text-6xl'>Welcome</h1>
+      <UserList />
     </div>
   )
 }
