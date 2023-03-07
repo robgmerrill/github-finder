a35b97c3280260e6dd8a438df8f01348eb9b06b2
Rob Merrill <robgmerrill@gmail.com>
2023-03-06 16:07:30 -0800

    add spiner component

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import Spinner from '../layout/Spinner'
 
 function UserList() {
   const [users, setUsers ] = useState([])
-//   const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetchUsers();
@@ -16,21 +17,21 @@ function UserList() {
     }})
     const data = await response.json();
     setUsers(data)
-    // setIsLoading(false)
+    setIsLoading(false)
   }
 
   console.log(users)
-//   if (!isLoading) {
+  if (!isLoading) {
       return (
-        <div className='grid sm:grid-cols-1 lg:grid-cols-3'>
+        <div className='grid sm:grid-cols-1 gap-8 lg:grid-cols-3'>
          {users && users.map((user) => (
               <h3 key={user.login}>{user.login}</h3>
             ))}
         </div>
       )
-//   } else {
-//     return <p>...loading</p>
-//   }
+  } else {
+    return <Spinner />
+  }
 }
 
 export default UserList
