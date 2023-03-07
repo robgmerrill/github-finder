848aa7daeaeeee220bcf53223baa2698a614ef68
Rob Merrill <robgmerrill@gmail.com>
2023-03-06 16:10:25 -0800

    remove unessary file

--- src/components/users/UserResults.js
+++ src/components/users/UserResults.js
@@ -1,38 +1,37 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
+import Spinner from '../layout/Spinner'
 
-function UserResults() {
+function UserList() {
+  const [users, setUsers ] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
-
-useEffect(() => {
+  useEffect(() => {
     fetchUsers();
-}, [])
+  }, []) 
 
-    const fetchUsers = async() => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        })
 
-        const data = await response.json();
-        setUsers(data);
-        setLoading(false)
-    }
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    }})
+    const data = await response.json();
+    setUsers(data)
+    setIsLoading(false)
+  }
 
-    if (!loading) {
-        return (
-          <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-            {users.map((user) => (
+  console.log(users)
+  if (!isLoading) {
+      return (
+        <div className='grid sm:grid-cols-1 gap-8 lg:grid-cols-3'>
+         {users && users.map((user) => (
               <h3 key={user.login}>{user.login}</h3>
             ))}
-          </div>
-        )
-    } else {
-        return <h3>Loading...</h3>
-    }
+        </div>
+      )
+  } else {
+    return <Spinner />
+  }
 }
 
-export default UserResults
+export default UserList
